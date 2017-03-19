56cc78380ddc64dc2c825bdb63e58304b3b592a1
Zayn Rahman <zaynrahn@gmail.com>
2017-03-19 21:00:31 +0000

    added show page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @places = Place.paginate(:page => params[:page], per_page: 10)
+    @places = Place.paginate(:page => params[:page], per_page: 5)
   end
 
   def new
@@ -14,6 +14,9 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @place = Place.find(params[:id])
+  end
   
   private
 
