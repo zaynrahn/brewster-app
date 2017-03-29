db6f7ab1c492d8bfd877f731bdb33debd59011fe
Zayn Rahman <zaynrahn@gmail.com>
2017-03-29 04:06:19 +0000

    Modified mailer to be personalized

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@brewsterapp.com"
 
-  def comment_added
-    mail(to: "zaynrahn@gmail.com", 
-      subject: "A comment has been added to your place")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: "@place_owner.email", 
+      subject: "A comment has been added to #{@place.name}")
   end
 end
